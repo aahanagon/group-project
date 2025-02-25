535a4ae6b66aa2599f8e700c2d7a5dfd5eba034e
aahanagon <gondaliaaahana@gmail.com>
2025-02-25 00:10:24 +0000

    Implemented function to return whether a number is even or odd in problem3.py using UCASE

---
+++ problem3.py
@@ -0,0 +1,15 @@
+#Check if a number is even or odf
+#Check if there is a remainder after dividing by 2 if so then return false
+#no edge cases needed to address
+
+#if number remainder 2=0-return true
+#else return false
+
+def isEven(num):
+    if num%2==0:
+        return True
+    else:
+        return False
+    
+print(isEven(5))
+print(isEven(4))
